c0219eeeea08cbe50263e68b4f0ee303c1200ead
johannes <johannesmiesenhardt@gmail.com>
2025-12-03 08:23:20 +0100

    Day 3
    This was a fun one, the first one that actually had me thinking a bit.
    Part 1 is basically just a do-whatever problem, but the second part got
    me thinking. I initially tried to do the dfs but with integers, this
    turned out to be disfunctional, because a*10+b only works if you only
    concat two integers, if there are more then it fails, because of spaces
    in between, or so I think.
    I went with a string-based dfs, that is also memoized, as I saw that
    towards the end of a bank, you end up with recurring subproblems, thus
    memoization works wonders here.

---
+++ day3/main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func parse(input string) [][]int {
+	var banks [][]int
+
+	for line := range strings.SplitSeq(strings.TrimSpace(input), "\n") {
+		var bank []int
+		for num := range strings.SplitSeq(line, "") {
+			parsed, err := strconv.Atoi(num)
+			if err != nil {
+				log.Fatal(err)
+			}
+			bank = append(bank, parsed)
+		}
+		banks = append(banks, bank)
+	}
+
+	return banks
+}
+
+func part1(banks [][]int) int {
+	sol := 0
+	for _, bank := range banks {
+		max := 0
+		for a := 0; a < len(bank); a++ {
+			for b := a + 1; b < len(bank); b++ {
+				c := bank[a]*10 + bank[b]
+				if c > max {
+					max = c
+				}
+			}
+		}
+		sol += max
+	}
+	return sol
+}
+
+type state = struct {
+	remaining int
+	offset    int
+}
+
+func dfs(cache map[state]string, bank []int, s state) string {
+	sol, hit := cache[s]
+	if hit {
+		return sol
+	}
+
+	if len(bank)-s.offset < s.remaining {
+		return ""
+	}
+	if len(bank) == s.offset {
+		return ""
+	}
+	if s.remaining == 0 {
+		return ""
+	}
+
+	fst := bank[s.offset]
+
+	taken := fmt.Sprint(fst) + dfs(cache, bank, state{
+		remaining: s.remaining - 1,
+		offset:    s.offset + 1,
+	})
+	missed := dfs(cache, bank, state{
+		remaining: s.remaining,
+		offset:    s.offset + 1,
+	})
+
+	a, err := strconv.Atoi(taken)
+	if err != nil {
+		a = 0
+	}
+	b, err := strconv.Atoi(missed)
+	if err != nil {
+		b = 0
+	}
+
+	var res string
+	if a > b {
+		res = taken
+	} else {
+		res = missed
+	}
+
+	cache[s] = res
+	return res
+}
+
+func part2(banks [][]int) int {
+	sol := 0
+	for _, bank := range banks {
+		cache := make(map[state]string)
+		max := dfs(cache, bank, state{12, 0})
+		n, err := strconv.Atoi(max)
+		if err != nil {
+			log.Fatal(err)
+		}
+		sol += n
+	}
+	return sol
+}
+
+func main() {
+	content, err := os.ReadFile("./input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	banks := parse(string(content))
+
+	fmt.Printf("Part 1: %d\n", part1(banks))
+	fmt.Printf("Part 2: %d\n", part2(banks))
+}
